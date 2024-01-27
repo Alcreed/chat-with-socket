113e1bb15afa71f8e23c38fab474f2388d960500
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-01-27 16:40:56 -0500

    Usando base de datos terso

--- server/index.js
+++ server/index.js
@@ -1,27 +1,73 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
 const app = express();
 const server = createServer(app)
+
 const io = new Server(server, {
   connectionStateRecovery: {}
 })
 
-io.on('connection', (socket) => {
+const db = createClient({
+  url: "libsql://fine-steel-alcreed.turso.io",
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`)
+
+io.on('connection', async (socket) => {
   console.log('A user has connected!');
   
   socket.on('disconnect', () => {
     console.log('A user has disconnected!');
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg) // Broadcast --> emitir mensaje a todos los usuarios
+  socket.on('chat message', async (msg) => {
+    let result
+
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg }
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString()) // Broadcast --> emitir mensaje a todos los usuarios
   })
+
+  console.log(socket.handshake.auth);
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content,  row.id.toString())
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
 })
 
 app.use(logger('dev'))
